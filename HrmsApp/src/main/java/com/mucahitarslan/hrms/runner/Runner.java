fd6fa9d9ae7a4235dae44502bf2cefcaa23bc4df
mucahitarslaan <mucahitarslaan@gmail.com>
2022-12-25 22:14:12 +0300

    testing new branch

--- HrmsApp/src/main/java/com/mucahitarslan/hrms/runner/Runner.java
+++ HrmsApp/src/main/java/com/mucahitarslan/hrms/runner/Runner.java
@@ -6,7 +6,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class Runner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
